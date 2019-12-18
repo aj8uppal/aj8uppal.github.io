aa2949857f314a4f7aa12eb418acf9c6dd67c4da
A.J. Uppal <ajuppal@1x-nat-vl931-172-30-190-166.wireless.umass.edu>
2019-12-18 03:51:22 -0500

    player in center

--- socket-canvas/index.js
+++ socket-canvas/index.js
@@ -19,13 +19,14 @@ http.listen(3000, function(){
 //APP
 
 let users = {};
-let BOARD_WIDTH = 10000;
-let BOARD_HEIGHT = 7500;
+let BOARD_WIDTH = 100;
+let BOARD_HEIGHT = 100;
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.emit('info', {id: socket.id, users: users});
-  users[socket.id] = {x: -1, y: -1, radius: 15, id: socket.id, targetX: -1, targetY: -1};
+  newUser = {absX: Math.round(Math.random()*BOARD_WIDTH), absY: Math.round(Math.random()*BOARD_HEIGHT), radius: 15, id: socket.id, velX: 0, velY: 0};
+  socket.emit('info', {id: socket.id, users: users, user: newUser});
+  users[socket.id] = newUser
   console.log(users);
   socket.broadcast.emit('newUser', users[socket.id])
   socket.on('disconnect', function(){
@@ -36,10 +37,15 @@ io.on('connection', function(socket){
   });
   socket.on('updateClientCoords', function(data){
     try {
-      users[socket.id].targetX = data.targetX;
-      users[socket.id].targetY = data.targetY;
-      users[socket.id].x = data.x;
-      users[socket.id].y = data.y;
+      users[socket.id].velX = data.velX;
+      users[socket.id].velY = data.velY;
+      // console.log(data.velX+" "+data.velY);
+      // if(data.velX === 0 && data.velY === 0){
+        users[socket.id].absX = data.absX;
+        users[socket.id].absY = data.absY;
+      // }
+      // users[socket.id].x = data.x;
+      // users[socket.id].y = data.y;
       socket.broadcast.emit('userMoved', users[socket.id]);
     }catch(e){
       //;
--- socket-canvas/public/main.js
+++ socket-canvas/public/main.js
@@ -27,16 +27,19 @@ $(document).ready(function(){
   canvas.height = window.innerHeight;
 
   let circleRadius = 15;
-  let mouseX = canvas.width/2-circleRadius/2;
-  let mouseY = canvas.height/2-circleRadius/2;
-  let player = {x: mouseX, y: mouseY, targetX: mouseX, targetY: mouseY, radius: circleRadius, color: "navy"};
+
+  let center = {x: canvas.width/2, y: canvas.height/2};
+
+  let mouseX = center.x;
+  let mouseY = center.y;
+  let player = {x: mouseX, y: mouseY, velX: 0, velY: 0, absX: -1, absY: -1, radius: circleRadius, color: "navy"};
   let speed = 10;
   let fps = 30;
 
   let BOARD_WIDTH = 10000;
   let BOARD_HEIGHT = 7500;
 
-  let offset = {x: 1000, y: 1000};
+  let offset;
 
   let circles = [];
 
@@ -51,40 +54,51 @@ $(document).ready(function(){
     elapsed = now - then;
     if(elapsed > fpsInterval){
       ctx.clearRect(0, 0, canvas.width, canvas.height);
+      console.log(circles.length);
       for(let c = 0; c < circles.length; ++c){
         let circle = circles[c];
-        dX = circle.targetX - circle.x;
-        dY = circle.targetY - circle.y;
-        if(dX === 0){
-          if(Math.abs(dY) > speed){
-            circle.y+=dY > 0 ? speed : -speed;
-          }
-        }else if(dX**2 + dY**2 > speed*2){
-          theta = Math.atan2(dY, dX);
-          circle.x+=speed*Math.cos(theta)
-          circle.y+=speed*Math.sin(theta)
-        }else{
-          circle.x = circle.targetX;
-          circle.y = circle.targetY;
+        if(circle.absX < offset.x || circle.absY < offset.y || circle.absX > offset.x+canvas.width || circle.absY > offset.y+canvas.height){
+          // console.log("bad");
+          continue;
         }
+        // debugger;
+        // console.log("good");
+        // circle.x = circle.absX - offset.x;
+        // circle.y = circle.absY - offset.y;
+        circle.x+=(circle.velX-player.velX);
+        circle.y+=(circle.velY-player.velY);
+        // dX = circle.targetX - circle.x;
+        // dY = circle.targetY - circle.y;
+        // if(dX === 0){
+        //   if(Math.abs(dY) > speed){
+        //     circle.y+=dY > 0 ? speed : -speed;
+        //   }
+        // }else if(dX**2 + dY**2 > speed*2){
+        //   theta = Math.atan2(dY, dX);
+        //   circle.x+=speed*Math.cos(theta)
+        //   circle.y+=speed*Math.sin(theta)
+        // }else{
+        //   circle.x = circle.targetX;
+        //   circle.y = circle.targetY;
+        // }
         drawCircle(circle);
       }
-      dX = player.targetX - player.x;
-      dY = player.targetY - player.y;
-      if(dX === 0){
-        if(Math.abs(dY) > speed){
-          player.y+=dY > 0 ? speed : -speed;
-        }
-      }else if(dX**2 + dY**2 > speed*2){
-        theta = Math.atan2(dY, dX);
-        player.x+=speed*Math.cos(theta)
-        player.y+=speed*Math.sin(theta)
-      }else{
-        player.x = player.targetX;
-        player.y = player.targetY;
-      }
+      // player.x+=player.velX;
+      // player.y+=player.velY;
+      // dX = mouseX - player.x;
+      // dY = mouseY - player.y;
+      // if( (dX**2+dY**2)**0.5 < player.radius){
+      //   player.velX = 0;
+      //   player.velY = 0;
+      //   socket.emit('updateClientCoords', {velX: player.velX, velY: player.velY, x: player.x, y: player.y})
+      // }
       // player.x = mouseX;
       // player.y = mouseY;
+      player.absX+=player.velX;
+      player.absY+=player.velY;
+      offset.x+=player.velX;
+      offset.y+=player.velY;
+
       drawCircle(player);
     }
   }
@@ -97,14 +111,45 @@ $(document).ready(function(){
   }
 
   $("#canvas").mousemove(function(e){
-    player.targetX = e.clientX+player.radius/2;
-    player.targetY = e.clientY+player.radius/2;
-    socket.emit('updateClientCoords', {targetX: player.targetX, targetY: player.targetY})
+    mouseX = e.clientX+player.radius/2;
+    mouseY = e.clientY+player.radius/2;
+    dX = mouseX - player.x;
+    dY = mouseY - player.y;
+    magnitude = (dX**2+dY**2)**0.5;
+    magnitude = parseFloat(magnitude.toFixed(3))
+    player.velX = magnitude < player.radius ? 0 : dX/150;
+    player.velY = magnitude < player.radius ? 0 : dY/150;
+    // if(dX === 0){
+    //   if(Math.abs(dY) > speed){
+    //     player.y+=dY > 0 ? speed : -speed;
+    //   }
+    // }else if(dX**2 + dY**2 > speed*2){
+    //   theta = Math.atan2(dY, dX);
+    //   player.x+=speed*Math.cos(theta)
+    //   player.y+=speed*Math.sin(theta)
+    // }else{
+    //   player.x = player.targetX;
+    //   player.y = player.targetY;
+    // }
+    console.log(player.absX+", "+player.absY);
+    socket.emit('updateClientCoords', {velX: player.velX, velY: player.velY, absX: player.x+offset.x, absY: player.y+offset.y})
   });
   // alert("connecting 2222...");
   socket.on('info', function(data){
     id = data.id;
     circles = Object.values(data.users);
+    // console.log(circles);
+    // console.log(id);
+    offset = {x: data.user.absX - center.x, y: data.user.absY - center.y};
+    player.absX = data.user.absX;
+    player.absY = data.user.absY;
+
+    circles.forEach((circle, index)=>{
+      circle.x = circle.absX - offset.x;
+      circle.y = circle.absY - offset.y;
+      // break;
+    })
+    // alert(player.absX+", "+player.absY);
     fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
@@ -126,8 +171,12 @@ $(document).ready(function(){
   socket.on('userMoved', function(user){
     circles.forEach((circle)=>{
       if(circle.id === user.id){
-        circle.targetX = user.targetX;
-        circle.targetY = user.targetY;
+        circle.absX = user.absX;
+        circle.absY = user.absY;
+        circle.velY = user.velY;
+        circle.velX = user.velX;
+        circle.x = circle.absX - offset.x;
+        circle.y = circle.absY - offset.y;
       }
     })
   });
